08fdf1c0f89660346c562625eb9db0bd110abe8a
Karthik B M <karthikbm0808@gmail.com>
2025-08-09 12:55:15 +0530

    comment_test

--- src/test/java/com/github/review/tracker/TrackerApplicationTests.java
+++ src/test/java/com/github/review/tracker/TrackerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
